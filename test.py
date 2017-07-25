24ebf368cf253967aed4afddc41c31c0c3f4c54c
ghpaetzold <ghpaetzold@outlook.com>
2017-07-25 14:18:48 +0100

    Updated test

--- test.py
+++ test.py
@@ -7,27 +7,24 @@ for i in range(0, len(files)-1):
 	for j in range(i+1, len(files)):
 		file1 = files[i].split('.')
 		file2 = files[j].split('.')
-		print file1, file2
+		if file1[0]==file2[0] and int(file1[2])<int(file2[2]):
+			file1 = '../newsela/corpora/truecased_corpus/'+files[i]
+			file2 = '../newsela/corpora/truecased_corpus/'+files[j]
+
+			print files[i], files[j]
 		
+			model = TFIDFModel([file1, file2], '../newsela/corpora/stop_words.txt')
 
-file1 = '../newsela/corpora/truecased_corpus/orioles-alvarez.en.0.txt'
-file2 = '../newsela/corpora/truecased_corpus/orioles-alvarez.en.1.txt'
+			paragraph_aligner = VicinityDrivenParagraphAligner(similarity_model=model, acceptable_similarity=0.3)
 
-model = TFIDFModel(['../newsela/corpora/truecased_corpus/orioles-alvarez.en.0.txt', '../newsela/corpora/truecased_corpus/orioles-alvarez.en.1.txt'], '../newsela/corpora/stop_words.txt')
+			sentence_aligner = VicinityDrivenSentenceAligner(similarity_model=model, acceptable_similarity=0.2, similarity_slack=0.05)
 
-paragraph_aligner = VicinityDrivenParagraphAligner(similarity_model=model, acceptable_similarity=0.3)
+			m = MASSAligner()
+			p1s = m.getParagraphsFromDocument(file1)
+			p2s = m.getParagraphsFromDocument(file2)
+			alignments, aligned_paragraphs = m.getParagraphAlignments(p1s, p2s, paragraph_aligner) 
 
-sentence_aligner = VicinityDrivenSentenceAligner(similarity_model=model, acceptable_similarity=0.2, similarity_slack=0.05)
-
-print 'Aligning paragraph...'
-m = MASSAligner()
-p1s = m.getParagraphsFromDocument(file1)
-p2s = m.getParagraphsFromDocument(file2)
-alignments, aligned_paragraphs = m.getParagraphAlignments(p1s, p2s, paragraph_aligner) 
-
-print 'Aligning sentences...'
-for a in aligned_paragraphs:
-	p1 = a[0]
-	p2 = a[1]
-	path = sentence_aligner.alignSentencesFromParagraphs(p1, p2)
-	print path
+			for a in aligned_paragraphs:
+				p1 = a[0]
+				p2 = a[1]
+				path = sentence_aligner.alignSentencesFromParagraphs(p1, p2)
